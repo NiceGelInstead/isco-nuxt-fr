33b9a9029302b3009a66bda2e7b29e5ff126f3e2
muhammadtegarrifqi <muhammadtegarrifqi@gmail.com>
2025-02-06 07:01:26 +0000

    Universal Hero Template

---
+++ components/HeroTemplate.vue
@@ -0,0 +1,24 @@
+// components/HeroTemplate.vue
+<template>
+
+<div class="w-full grid gap-6 grid-cols-1 md:grid-cols-2 items-center">
+<div>
+    <h1 class="isco-h3 mb-6">{{ title }}</h1>
+    <h2 class="isco-h2">{{ tagline }}</h2>
+</div>
+<div class="flex h-full lg:place-content-end">
+    <p class="max-w-md mt-auto">
+    {{ description }}
+    </p>
+</div>
+</div>
+
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+  tagline: String,
+  description: String
+});
+</script>
--- pages/Berita.vue
+++ pages/Berita.vue
@@ -2,17 +2,11 @@
 
 <section class="pt-32 pb-32">
     <div class="container">
-        <div class="w-full grid grid-cols-1 md:grid-cols-2 items-center">
-            <div>
-                <h1 class="isco-h3 mb-6">Berita & Artikel</h1>
-                <h2 class="isco-h2">Temukan Informasi Terkini <br> tentang Dunia Konstruksi</h2>
-            </div>
-            <div class="flex h-full place-content-end">
-                <p class="max-w-md mt-auto">
-                    Berita, pengumuman, dan wawasan terkini dari para kontraktor dan konsultan konstruksi terkemuka.
-                </p>
-            </div>
-        </div>
+        <HeroTemplate 
+            title="Berita & Artikel" 
+            tagline="Temukan Informasi Terkini Di Dunia Konstruksi" 
+            description="Berita, pengumuman, dan wawasan terkini seputar dunia kontraktor dan konsultan."
+        />
     </div>
 </section>
 
