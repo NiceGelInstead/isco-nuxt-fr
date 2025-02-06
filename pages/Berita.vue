c3788d71be3eaf9eb85dcb18908d5e16609b1a59
sophiealesyaa <sophiealesyaa@gmail.com>
2025-02-06 03:38:47 +0000

    x.com/adamwathan/status/1559250403547652097

---
+++ pages/Berita.vue
@@ -0,0 +1,19 @@
+<template>
+<section class="pt-32 pb-32">
+    <div class="isco-container">
+        <div class="w-full grid grid-cols-1 md:grid-cols-2 items-center">
+            <div>
+                <h1 class="isco-h3 mb-6">Berita & Artikel</h1>
+                <h2 class="isco-h2">Temukan Informasi Terkini <br> tentang Dunia Konstruksi</h2>
+            </div>
+            <div class="flex h-full place-content-end">
+                <p class="max-w-md mt-auto">
+                    Berita, pengumuman, dan wawasan terkini dari para kontraktor dan konsultan konstruksi terkemuka.
+                </p>
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+</template>
