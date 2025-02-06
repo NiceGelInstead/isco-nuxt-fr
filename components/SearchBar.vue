b538c200e072b54a00062c7daae87d673933e19c
muhammadtegarrifqi <muhammadtegarrifqi@gmail.com>
2025-02-06 04:46:37 +0000

    Add Search Function

---
+++ components/SearchBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+// Constantly Emit Results
+import { defineProps, defineEmits } from 'vue'
+defineProps<{ searchQuery: string }>()
+const emit = defineEmits(['update:searchQuery'])
+
+</script>
+
+
+<template>
+
+<div class="">
+    Cari Player
+</div>
+<input
+    :value="searchQuery"
+    @input="emit('update:searchQuery', ($event.target as HTMLInputElement).value)"
+    type="text"
+    placeholder="Player Name"
+    class="w-3xs p-1 border-b border-gray-800 focus:outline-none focus:ring-2 focus:ring-primary"
+/>
+
+</template>
---
+++ composables/useSearchPlayerName.ts
@@ -0,0 +1,14 @@
+import { ref, computed } from 'vue'
+import type { Player } from '~/types/playerTypes'
+
+export const useSearchPlayerName = (players: Player[]) => {
+  const searchQuery = ref('')
+  const getFilteredPlayers = computed(() => {
+    return players.length
+      ? players.filter(player =>
+          player.Name.toLowerCase().includes(searchQuery.value.toLowerCase())
+        )
+      : []
+  })
+  return { searchQuery, getFilteredPlayers }
+}
