d0edaab3b320a6d694b8484ca098f83a4dc0481c
Adjistyr <86603800+Adjistyr@users.noreply.github.com>
2025-02-07 20:55:36 +0700

    Add Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,73 @@
 <template>
-    <footer>
-        <p>&copy; 2024 ISCO Group</p>
-    </footer>
-</template>
+    <footer class="bg-secondary">
+      <div class="container py-14 md:py-16 flex flex-col md:flex-row justify-between items-center">
+        <!-- Logo & Deskripsi + Sosial Media -->
+        <div class="w-full md:w-1/4">
+          <div class="flex items-center gap-3 mb-4">
+            <img src="@/assets/images/logo-IscoGroup.png" alt="Logo" class="size-16" />
+            <nuxt-link to="/" class="text-2xl font-bold text-white">Isco Group</nuxt-link>
+          </div>
+          <p class="text-base font-medium mb-4 text-white text-left">
+            ISCO GROUP adalah perusahaan yang bergerak di konsultasi konstruksi dan legalitas sertifikasi.
+          </p>
+          <div class="flex gap-4">
+            <nuxt-link to="/"><i class="fab fa-facebook text-white text-xl hover:underline"></i></nuxt-link>
+            <nuxt-link to="/"><i class="fab fa-instagram text-white text-xl hover:underline"></i></nuxt-link>
+            <nuxt-link to="/"><i class="fab fa-linkedin text-white text-xl hover:underline"></i></nuxt-link>
+          </div>
+        </div>
+  
+        <!-- Tentang Kami & Layanan (dalam satu row di mobile) -->
+        <div class="w-full md:w-1/2 flex flex-row justify-between md:justify-around mt-8 md:mt-0">
+          <div>
+            <nuxt-link to="/" class="text-2xl font-bold text-white hover:underline">Layanan</nuxt-link>
+                <div class="mt-4">
+                    <p class="footer-p">ISO Series</p>
+                    <p class="footer-p">Sertifikat BNSP</p>
+                    <p class="footer-p">Sertifikat SBU</p>
+                    <p class="footer-p">Sertifikat SBK</p>
+                    <p class="footer-p">Lihat Semua</p>
+                </div>
+          </div>
+          <div>
+            <nuxt-link to="/" class="text-2xl font-bold text-white hover:underline">Tentang Kami</nuxt-link>
+            <div class="mt-4">
+                <p><nuxt-link to ="/" class="footer-p">Profil </nuxt-link></p>
+                <p><nuxt-link to ="/" class="footer-p">Alamat</nuxt-link></p>
+                <p><nuxt-link to ="/" class="footer-p">Anggota</nuxt-link></p>
+                <p><nuxt-link to ="/" class="footer-p">Blog & Artikel</nuxt-link></p>
+            </div>
+          </div>
+        </div>
+       
+        
+        <!-- Kontak -->
+        <div class="w-full md:w-1/4 mt-8 md:mt-0">
+          <nuxt-link to="/" class="text-2xl font-bold text-white hover:underline">Informasi Kontak</nuxt-link>
+          <div class="flex items-center gap-3 mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8 text-white">
+            <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+            </svg>
 
-<script>
-export default {
-    name: 'Footer'
-}
-</script>
+            <p class="footer-p">Jalan Rahasia Barat No. 2, Banget, Kota Jayajarta, 10150</p>
+          </div>
+          <div class="flex items-center gap-3 mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-white">
+            <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clip-rule="evenodd" />
+            </svg>
+
+            <p class="footer-p">+62 8123-2321-1234</p>
+          </div>
+          <div class="flex items-center gap-3 mt-4">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 text-white">
+            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+            <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+            </svg>
+
+            <p class="footer-p">iscogroup@gmail.com</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </template>
+  
