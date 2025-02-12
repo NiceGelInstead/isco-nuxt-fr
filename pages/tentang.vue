e683c626fb4bb6d0b63ecfb1eec6a30baf3d7fdc
muhammadtegarrifqi <muhammadtegarrifqi@gmail.com>
2025-02-12 01:58:01 +0000

    initial content /tentang

---
+++ pages/tentang.vue
@@ -0,0 +1,95 @@
+<template>
+    <section class="pt-32 pb-32">
+      <div class="container">
+        <HeroTemplate
+          title="Tentang Kami"
+          tagline="Tentang Isco Group"
+          description="Mempersatukan kontraktor Unggulan di seluruh Indonesia"
+        />
+      </div>
+    </section>
+  
+    <section>
+      <div class="container">
+       <div class="grid grid-cols-2 gap-16">
+        <img
+           src="@/assets/images/test2.png"
+           class="h-100 w-full object-contain" alt="poto"
+        >
+        <div class="flex flex-col gap-4">
+            <h2 class="isco-h3">
+                Sejarah
+            </h2>
+            <h3 class="isco-h2">
+                Generasi Muda adalah Pilar Pembangunan Bangsa
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo veniam ullam labore quia! Inventore mollitia cupiditate est amet dolor, similique dicta at perspiciatis neque molestias blanditiis cum facere aperiam voluptates veritatis ipsam?
+            </p>
+        </div>
+       </div>
+      </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <h2 class="isco-h3 mb-4">
+                Asosiasi Kami
+            </h2>
+            <h3 class="isco-h2 mb-2">
+                Asosiasi
+            </h3>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur animi dolor unde, ipsum officiis alias ex aliquam dolorem cumque nostrum tempore voluptatem dicta!
+            </p>
+            <div class="flex gap-8 mt-8">
+                <img
+           src="@/assets/images/test2.png"
+           class="h-28 w-full object-contain" alt="poto"
+        >
+        <img
+           src="@/assets/images/test2.png"
+           class="h-28 w-full object-contain" alt="poto"
+        >
+        <img
+           src="@/assets/images/test2.png"
+           class="h-28 w-full object-contain" alt="poto"
+        >
+        <img
+           src="@/assets/images/test2.png"
+           class="h-28 w-full object-contain" alt="poto"
+        >
+        <img
+           src="@/assets/images/test2.png"
+           class="h-28 w-full object-contain" alt="poto"
+        >
+            </div>
+        </div>
+    </section>
+
+    <section>
+      <div class="container">
+       <div class="grid grid-cols-2 gap-16">
+        <img
+           src="@/assets/images/test2.png"
+           class="h-100 w-full object-contain" alt="poto"
+        >
+        <div class="flex flex-col-reverse gap-4">
+            <h2 class="isco-h3">
+                Sejarah
+            </h2>
+            <h3 class="isco-h2">
+                Generasi Muda adalah Pilar Pembangunan Bangsa
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo veniam ullam labore quia! Inventore mollitia cupiditate est amet dolor, similique dicta at perspiciatis neque molestias blanditiis cum facere aperiam voluptates veritatis ipsam?
+            </p>
+        </div>
+       </div>
+      </div>
+    </section>
+
+  </template>
+  
+  <script setup lang="ts"></script>
+  
