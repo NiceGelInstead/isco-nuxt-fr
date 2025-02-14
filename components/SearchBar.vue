db4da5dc786103db901eacd52ddebd47698dce67
Adjistyr <86603800+Adjistyr@users.noreply.github.com>
2025-02-14 23:37:23 +0700

    Update Component SearchBar

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -13,14 +13,17 @@ defineProps<{
 </script>
 
 <template>
+  <div class="mt-0 md:mt-16">
   <div class="mb-1.5 font-semibold">{{ searchText }}</div>
   <input
     :value="searchQuery"
     type="text"
     :placeholder="searchPlaceholder"
-    class="w-3xs mb-4 p-1 border-b border-gray-800 focus:outline-none focus:ring-2 focus:ring-primary"
+    class="w-3xs mb-4 p-1 border-b border-gray-800 focus:outline-none focus:border-b-2 focus:border-b-primary"
     @input="
       emit('update:searchQuery', ($event.target as HTMLInputElement).value)
     "
   >
+  </div>
+
 </template>
