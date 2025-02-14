d73f476a478a541bbb4873da34937fa38d71e9fd
Adjistyr <86603800+Adjistyr@users.noreply.github.com>
2025-02-14 23:40:36 +0700

    add page pelayanan

---
+++ pages/pelayanan.vue
@@ -0,0 +1,29 @@
+<template>
+    <main id="Layanan Isco Group">
+      <section class="mt-4 md:mt-16">
+        <div class="container">
+            <HeroTemplate
+            title="Layanan Kami"
+            tagline="Memajukan Kontraktor di Indonesia Bersama  ISCO Group"
+            description="Kami menawarkan pelayanan lengkap untuk Kontraktor Jasa Konstruksi dan Jasa Konsultasi."
+            />
+        </div>
+      </section>
+      <section>
+          <div class="container mt-0 pt-0">
+            <PelayananCarousel />
+          </div>
+      </section>
+
+      <section id="Hubungi Kami Segera">
+      <BlockCTA />
+    </section>
+
+    </main>
+  
+</template>
+
+<script setup lang="ts">
+
+
+</script>
