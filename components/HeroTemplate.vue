a29f16d3174d92e1c68b980b9cce2442b78d1014
Adjistyr <86603800+Adjistyr@users.noreply.github.com>
2025-02-14 23:36:15 +0700

    Update HeroTemplate

--- components/HeroTemplate.vue
+++ components/HeroTemplate.vue
@@ -3,11 +3,11 @@
 
 <div class="w-full grid gap-6 grid-cols-1 md:grid-cols-2 items-center">
 <div class="">
-    <h1 class="isco-h3 mb-6">{{ title }}</h1>
-    <h2 class="isco-h2">{{ tagline }}</h2>
+    <h1 class="text-left text-accent mb-6">{{ title }}</h1>
+    <h2 class="isco-h2 text-left">{{ tagline }}</h2>
 </div>
 <div class="flex h-full lg:place-content-end">
-    <p class="max-w-md md:max-w-lg mt-auto">
+    <p class="text-left max-w-md md:max-w-lg mt-auto">
     {{ description }}
     </p>
 </div>
