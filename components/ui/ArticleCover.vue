9da9d74927fbdbb2ea9a8a4a5252d677584c534b
muhammadtegarrifqi <muhammadtegarrifqi@gmail.com>
2025-02-19 06:14:17 +0000

    articleTypes

---
+++ components/ui/ArticleCover.vue
@@ -0,0 +1,19 @@
+<template>
+    <img :src="imageUrl" alt="" :class="cssClass" />
+  </template>
+  
+  <script setup>
+  // URL strapi
+  const config = useRuntimeConfig();
+  const strapiUrl = config.public.strapiUrl;
+  
+  // Catch image and classes
+  const props = defineProps({
+    article: Object,
+    cssClass: String,
+  });
+  const imageUrl = props.article?.cover?.url
+    ? strapiUrl + props.article.cover.url
+    : "";
+  </script>
+  
---
+++ types/articleTypes.ts
@@ -0,0 +1,50 @@
+// types/playerTypes.ts
+
+// Default in case of nulls
+  export const defaultCover: cover = {
+    url: "", // this has to be above defaultPlayer for whatever reason XD
+  };
+  export const defaultAuthor: author = {
+    name: "Isco Group", // this has to be above defaultPlayer for whatever reason XD
+  };
+  export const defaultCategory: category = {
+    name: "No Category", // this has to be above defaultPlayer for whatever reason XD
+  };
+  export const defaultArticle: Article = {
+    id: 0,
+    documentId: "NULL",
+    title: "No Title",
+    description: "No Description",
+    slug: "missing-slug",
+    cover: defaultCover,
+    author: defaultAuthor,
+    category: defaultCategory,
+
+  };
+  
+  export interface Article {
+    id: number;
+    documentId: string;
+    title: string;
+    description: string;
+    slug: string;
+    cover: cover | null;
+    author: author | null;
+    category: category | null;
+
+  }
+  interface cover {
+    url: string;
+  }
+  interface author {
+    name: string;
+  }
+  interface category {
+    name: string;
+  }
+  
+  // Export end products (lmao)
+  export interface ApiResponse<T> {
+    data: T;
+  }
+  
