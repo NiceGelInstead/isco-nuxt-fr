023034f33c1d6e0f3b87f50b04e4327b02945087
muhammadtegarrifqi <muhammadtegarrifqi@gmail.com>
2025-02-27 01:27:56 +0000

    Side CTA

---
+++ components/ui/SideCTA.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col px-6 py-8 gap-2.5 bg-secondary rounded-xl sticky">
+        <span class="isco-h4 text-white">
+            Masih Ada Pertanyaan? Konsultasi Gratis Sekarang!
+        </span>
+        <ButtonCTAIcon button-text="Konsultasi Gratis" />
+    </div>
+</template>
+
+<script></script>
