ba78661a8e5115c814d8e5aa68ef68d141eb6166
muhammadtegarrifqi <muhammadtegarrifqi@gmail.com>
2025-03-07 07:26:45 +0000

    Stub

--- components/HeroTemplate.vue
+++ components/HeroTemplate.vue
@@ -1,12 +1,12 @@
 // components/HeroTemplate.vue Lama
 <template>
-  <section id="hero" class="py-0 pt-[calc(var(--header-height))]">
-    <div class="isco-container py-16 grid grid-cols-1 md:grid-cols-12 gap-8">
+  <section id="hero" class="py-0 mt-[calc(var(--header-height))]">
+    <div class="isco-container my-16 grid grid-cols-1 md:grid-cols-12 gap-8">
       <div class="col-span-6">
-        <h1 class="isco-h4 text-accent mb-4 md:mb-6">{{ title }}</h1>
+        <h1 class="isco-h4 text-primary mb-4 md:mb-6">{{ title }}</h1>
         <h2 class="isco-h2 text-left">{{ tagline }}</h2>
       </div>
-      <div class="col-span-2">
+      <div class="hidden md:col-span-2">
         .
       </div>
       <div class="flex h-full col-span-4">
--- pages/layanan.vue
+++ pages/layanan.vue
@@ -1,21 +1,15 @@
 <template>
   <main id="layanan">
-    <section class="mt-4 md:mt-16">
-      <div class="container">
         <HeroTemplate
           title="Layanan Kami"
-          tagline="Memajukan Kontraktor di Indonesia Bersama  ISCO Group"
+          tagline="Memajukan Kontraktor Di Indonesia Bersama ISCO Group"
           description="Kami menawarkan pelayanan lengkap untuk Kontraktor Jasa Konstruksi dan Jasa Konsultasi."
         />
-      </div>
-    </section>
     <section>
       <div class="container mt-0 pt-0">
-        <PelayananCarousel />
+        <PelayananCarousel class="grid-cols-1 md:grid-cols-3"/>
       </div>
     </section>
-
-    <section id="hubungi-kami">
       <BlockCTA />
     </section>
   </main>
