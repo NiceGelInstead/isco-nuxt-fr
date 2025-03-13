d8ca56641d6ecc698f1b1f8611d04f05ca44fe5c
muhammadtegarrifqi <muhammadtegarrifqi@gmail.com>
2025-03-13 01:18:29 +0000

    Add Timeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,13 @@ export default defineNuxtConfig({
     },
   ],
   compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   runtimeConfig: {
     strapiUrl: "https://staging.testjellysuper.my.id",
     public: {
